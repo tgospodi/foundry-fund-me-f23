61f328f0de06ecb47d96bcca93f6cc70e162478f
tgospodi <gospodinovtodor@gmail.com>
2023-12-19 17:17:39 +0200

    final lottery commit

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -15,7 +15,11 @@ contract HelperConfig is Script {
         uint64 subscriptionId;
         uint32 callbackGasLimit;
         address link;
+        uint256 deployerKey;
     }
+
+    uint256 public constant DEFAULT_ANVIL_KEY =
+        0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
 
     NetworkConfig public activeNetworkConfig;
 
@@ -28,16 +32,17 @@ contract HelperConfig is Script {
         }
     }
 
-    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+    function getSepoliaEthConfig() public view returns (NetworkConfig memory) {
         return
             NetworkConfig({
-                entranceFee: 0.1 ether,
+                entranceFee: 0.01 ether,
                 interval: 30,
                 vrfCoordinator: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625,
                 gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c, //also called key hash in Chainlink VRF site
-                subscriptionId: 0, // currently set to 0 but needs update with a subid
+                subscriptionId: 7939, // currently set to 0 but needs update with a subid
                 callbackGasLimit: 500000,
-                link: 0x779877A7B0D9E8603169DdbD7836e478b4624789 // 500,000 gas
+                link: 0x779877A7B0D9E8603169DdbD7836e478b4624789, // 500,000 gas
+                deployerKey: vm.envUint("PRIVATE_KEY")
             });
     }
 
@@ -65,7 +70,8 @@ contract HelperConfig is Script {
                 gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c, //also called key hash in Chainlink VRF site
                 subscriptionId: 0, // our script will add this
                 callbackGasLimit: 500000,
-                link: address(link) // 500,000 gas
+                link: address(link), // 500,000 gas
+                deployerKey: DEFAULT_ANVIL_KEY
             });
     }
 }
---
+++ test/integration/InteractionsTest.t.sol
@@ -0,0 +1,8 @@
+// unit test
+// integration test
+// forked test
+// staging <- run tests on a mainnet/testnet
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
